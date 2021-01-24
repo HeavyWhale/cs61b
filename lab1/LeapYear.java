5b63de4812aa7710689b85c8a9c3a4d9fb682c7f
Leo Chen <rzco0717@gmail.com>
2021-01-24 17:36:10 -0500

    completed second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,12 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Leo Chen
  */
 public class LeapYear {
+
+    /** Returns True if given year is a leap year; false otherwise */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) ? true : ((year % 4 == 0) && (year % 100 != 0));
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
